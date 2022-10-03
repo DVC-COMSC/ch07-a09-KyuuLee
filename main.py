97489c4f93ad94d1df42f86b39d4a7e445d7daf4
Kyuu <69904775+KyuuLee@users.noreply.github.com>
2022-10-02 18:17:38 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+
 numbers = [     [99, 11, 66, 86, 55],
                [44, 21, 65, 88, 24],
                [33, 77, 32, 33, 34]]
@@ -13,6 +14,21 @@ for i in range(rnum):
 	print (rsum, end=' ')
 print()
 
-# ******************************
-# Make your Code
-# ******************************
+print ('Sum of columns: ', end=' ')
+for i in range(cnum):
+	csum = 0
+	for j in range(rnum):	
+		csum += numbers[j][i]
+	print (csum, end=' ')
+print ()
+
+
+for i in range(rnum):
+	rsum = sum(numbers[i])
+	if i == 0 or tmax < rsum:
+		tmax = rsum
+		idx = i
+print ('The row that has greatest sum: ', idx)
+
+maxnum = [ max(numbers[i]) for i in range(rnum) ]
+print ('The greatest number: ', max(maxnum))
